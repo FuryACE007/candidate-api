4099c08d92ec5a5bd22789f0c3b3135211eb33f3
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-06-08 07:46:51 +0530

    added caching, but have errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CandidatesModule } from './candidates/candidates.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CandidatesModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: 'memory',
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        max: 1000,
+        ttl: configService.get('CACHE_TTL'),
+      }),
+    }),
+    CandidatesModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
