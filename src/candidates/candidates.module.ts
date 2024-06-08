e96aacb68a1536dfd124980bb16c576d2d96cdb8
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-06-08 11:38:27 +0530

    fixed attempt

--- src/candidates/candidates.module.ts
+++ src/candidates/candidates.module.ts
@@ -6,6 +6,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   providers: [CandidatesService],
-  controllers: [CandidatesController]
+  controllers: [CandidatesController],
 })
 export class CandidatesModule {}
